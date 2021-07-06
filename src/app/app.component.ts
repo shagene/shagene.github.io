dd80ac59e839cf160b20fbd247bb84b8a1651580
Steven Hagene <hagenes@americas.cshare.net>
2021-07-06 15:27:40 -0400

    update to structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
 import { BreakpointObserver } from '@angular/cdk/layout';
 
 import { MatSidenav } from '@angular/material/sidenav';
@@ -8,7 +8,7 @@ import { MatSidenav } from '@angular/material/sidenav';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
   title = 'angMatDemo';
@@ -18,14 +18,18 @@ export class AppComponent {
   constructor(private observer: BreakpointObserver) {}
 
   ngAfterViewInit() {
-    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
-      if (res.matches) {
-        this.sidenav.mode = 'over';
-        this.sidenav.close();
-      } else {
-        this.sidenav.mode = 'side';
-        this.sidenav.open();
-      }
-    });
+    // this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+    //   if (res.matches) {
+    //     this.sidenav.mode = 'over';
+    //     this.sidenav.close();
+    //   } else {
+    //     this.sidenav.mode = 'side';
+    //     this.sidenav.open();
+    //   }
+    // });
+  }
+
+  ngOnInit(): void {
+    // console.log(navigator.userAgent);
   }
 }
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
@@ -6,6 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
+  appStructureCode = `<div class="wrapper">
+      <app-header></app-header>
+      <div class="content">
+        <app-appbar></app-appbar>
+        <div class="right-half">
+          <div style="padding: 50px">
+            <router-outlet></router-outlet>
+          </div>
+        </div>
+      </div>
+      <app-footer></app-footer>
+</div>`;
 
   opened = false;
   log(state: any) {
@@ -13,9 +28,8 @@ export class SidenavComponent implements OnInit {
   }
 
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
