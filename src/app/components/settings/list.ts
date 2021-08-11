351766023dfe1e7a4caa500c9ce79f6426a8c951
shagene <hagenes@americas.cshare.net>
2021-08-11 12:21:37 -0400

    testing accordion

---
+++ src/app/components/settings/list.ts
@@ -0,0 +1,5 @@
+export class List {
+  title!: string;
+  children: any;
+  hide!: boolean;
+}
--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,13 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+//our root app component
+import {Component, VERSION, OnInit} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+import { List } from './list';
 
+const LIST: List[] = [
+    {
+        title: 'Menu 1',
+        hide : true,
+        children: [],
+    },
+    {
+        title: 'Menu 2',
+        hide : true,
+        children: [{
+            title: 'Sub Menu 2',
+            hide : true,
+            children: [{
+                title: 'Sub Sub Menu 2',
+                hide : true,
+                children: [{
+                    title: 'Sub Sub Menu 2, Sibling 1',
+                    hide : true,
+                    children: []
+                },
+                {
+                    title: 'Sub Sub Sub Menu 2, Sibling 2',
+                    hide : true,
+                    children: []
+                }]
+            }]
+        }]
+
+    },
+    {
+        title: 'Menu 3',
+        hide : true,
+        children: []
+    }
+];
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  name:string;
+  constructor() {
+    this.name = `Angular! v${VERSION.full}`
+  }
 
-  constructor() { }
+  title = 'Nested Accordion';
+  list = LIST;
+  selectedList: List = new List;
+  onSelect(list: List): void {
+    list.hide = !list.hide;
+    this.selectedList = list;
+}
 
   ngOnInit(): void {
   }
